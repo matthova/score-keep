49ff492e666db27decb6ddb8e9e690fdc0829cee
matthova <matthova87@gmail.com>
2017-07-17 23:42:28 -0700

    Loading players from a hard coded array

--- client/main.js
+++ client/main.js
@@ -2,9 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 
+const players = [
+  {
+    name: 'Kaitlyn',
+    score: 100,
+    _id: '1',
+  },
+  {
+    name: 'Matthew',
+    score: -1,
+    _id: '2',
+  },
+  {
+    name: 'Peter',
+    score: 50,
+    _id: '3',
+  },
+];
+
+const renderPlayers = function(players = []) {
+  return players.map(player => (
+    <p key={player._id}>{player.name} has {player.score} point{Math.abs(Number(player.score)) === 1 ? '' : 's' }.</p>
+  ));
+};
+
 Meteor.startup(function() {
-  debugger;
+  const title = 'Score Keep';
   let name = 'Meee';
-  let jsx = <p>Cooool, {name}</p>;
+
+  let jsx = (
+    <div>
+      <h1>{title}</h1>
+      {renderPlayers(players)}
+    </div>
+  );
   ReactDOM.render(jsx, document.getElementById('app'));
 });
--- server/main.js
+++ server/main.js
@@ -2,9 +2,4 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(function() {
   console.log('Server is started up');
-
-  setInterval(() => {
-    debugger;
-    console.log('eyyy');
-  }, 5000);
 });

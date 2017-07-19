898af381c5fbd76f332b4681cf077af096007052
matthova <matthova87@gmail.com>
2017-07-19 00:25:26 -0700

    Added conditional prompt for adding your first player

--- imports/ui/PlayerList.js
+++ imports/ui/PlayerList.js
@@ -4,10 +4,18 @@ import PropTypes from 'prop-types';
 import Player from './Player';
 
 export default class PlayerList extends React.Component {
+  renderPlayers() {
+    if (this.props.players.length > 0) {
+      return this.props.players.map(player => <Player key={player._id} player={player}/>);
+    }
+
+    return <p>Add your first player</p>;
+  }
+
   render() {
     return (
       <div>
-        {this.props.players.map(player => <Player key={player._id} player={player}/>)}
+        {this.renderPlayers()}
       </div>
     );
   }

8248aaedd0d757cfcdb59e62c65e767e1c0db5b1
matthova <matthova87@gmail.com>
2017-07-18 23:50:39 -0700

    Moved add player to be a class function

--- imports/ui/AddPlayer.js
+++ imports/ui/AddPlayer.js
@@ -2,23 +2,22 @@ import React from 'react';
 
 import { Players } from './../api/players';
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  const playerName = e.target.playerName.value;
-  if (playerName) {
-    e.target.playerName.value = '';
-
-    Players.insert({
-      name: playerName,
-      score: 0,
-    });
-  }
-}
-
 export default class AddPlayer extends React.Component {
+  handleSubmit(e) {
+    e.preventDefault();
+    const playerName = e.target.playerName.value;
+    if (playerName) {
+      e.target.playerName.value = '';
+      Players.insert({
+        name: playerName,
+        score: 0,
+      });
+    }
+  }
+
   render() {
     return (
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={this.handleSubmit.bind(this)}>
         <input type="text" name="playerName" placeholder="Player Name"/>
         <button>Add Player</button>
       </form>

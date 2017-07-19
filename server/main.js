7f24b86b9652c58952f0e72784a103c3d4dd8ed9
matthova <matthova87@gmail.com>
2017-07-18 20:01:52 -0700

    arrow function example

--- server/main.js
+++ server/main.js
@@ -1,6 +1,18 @@
 import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
+
 import { Players } from './../imports/api/players';
 
 Meteor.startup(function () {
-  // console.log(Players.find().fetch());
+  const user = {
+    name: 'Kaitlyn',
+    sayHi () {
+      setTimeout(() => {
+        console.log(this.name);
+      }, 1000);
+    }
+  }
+
+  user.sayHi(1, 2);
+
 });

f61771e8e8b2205b6d8a873052160cdea52b60f4
matthova <matthova87@gmail.com>
2017-07-19 00:34:22 -0700

    Refactored App Component

--- client/main.js
+++ client/main.js
@@ -3,22 +3,12 @@ import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import { Tracker } from 'meteor/tracker';
 
-import PlayerList from './../imports/ui/PlayerList';
-import TitleBar from './../imports/ui/TitleBar';
-import AddPlayer from './../imports/ui/AddPlayer';
 import { Players } from './../imports/api/players';
+import App from './../imports/ui/App';
 
 Meteor.startup(() => {
   Tracker.autorun(() => {
     const players = Players.find().fetch();
-
-    let jsx = (
-      <div>
-        <TitleBar title="Score Keep" subtitle="Created by Matt Hova"/>
-        <AddPlayer/>
-        <PlayerList players={players}/>
-      </div>
-    );
-    ReactDOM.render(jsx, document.getElementById('app'));
+    ReactDOM.render(<App players={players}/>, document.getElementById('app'));
   });
 });
---
+++ imports/ui/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import PlayerList from './PlayerList';
+import TitleBar from './TitleBar';
+import AddPlayer from './AddPlayer';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <TitleBar title="Score Keep" subtitle="Created by Matt Hova"/>
+        <AddPlayer/>
+        <PlayerList players={this.props.players}/>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  players: PropTypes.array.isRequired
+};
